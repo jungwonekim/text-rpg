fef3c5a592cb5ee7d1d11f9de42b0404d7290e1c
jungwonekim <jungwon.wk@gmail.com>
2026-01-08 13:33:50 +0900

    feat(player): 직업별 초기 스텟 설정 및 테스트 코드 추가P

--- Models/Player.cs
+++ Models/Player.cs
@@ -14,15 +14,60 @@ public class Player: Character
     #region 생성자
     public Player(string name, JobType job) : base(
         name: name,
-        maxHp: 100,
-        maxMp: 50,
-        attackPower: 20,
-        defense: 10,
-        level: 1)
+        maxHp: GetInitHp(job),
+        maxMp: GetInitMp(job),
+        attackPower: GetInitAttack(job),
+        defense: GetInitDefense(job),
+        level: 1
+        )
     {
         Job = job;
         Gold = 1000;
     }
+    #endregion
+
+    #region 직업별 초기 스텟
+
+    private static int GetInitHp(JobType job)
+    {
+        switch (job)
+        {
+            case JobType.Warrior: return 150;
+            case JobType.Archer: return 100;
+            case JobType.Wizard: return 80;
+            default: return 100;
+        }
+    }
+
+    private static int GetInitMp(JobType job)
+    {
+        switch (job)
+        {
+            case JobType.Warrior: return 30;
+            case JobType.Archer: return 50;
+            case JobType.Wizard: return 100;
+            default: return 30;
+        }
+    }
+
+    private static int GetInitAttack(JobType job) =>
+        job switch
+        {
+            JobType.Warrior => 20,
+            JobType.Archer => 30,
+            JobType.Wizard => 40,
+            _ => 20
+        };
+
+    private static int GetInitDefense(JobType job) =>
+        job switch 
+        {
+            JobType.Warrior => 15,
+            JobType.Archer => 10,
+            JobType.Wizard => 50,
+            _ => 15
+        };
+
     #endregion
 
 }
