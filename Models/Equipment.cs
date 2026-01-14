b89758130f523b92fdd846e490a6a751466a526a
jungwonekim <jungwon.wk@gmail.com>
2026-01-14 10:39:47 +0900

    feat(equipment): 기본 장비 방어구 방어력 증가 및  초기 장비 작용 기능 추가

--- Models/Equipment.cs
+++ Models/Equipment.cs
@@ -60,7 +60,7 @@ public class Equipment: Item
         switch (armorType)
         {
             case "천갑옷":
-                return new Equipment("천갑옷", "기본 방어구", 100, EquipmentSlot.Armor, 0, 5);
+                return new Equipment("천갑옷", "기본 방어구", 100, EquipmentSlot.Armor, 0, 10);
             case "철갑옷":
                 return new Equipment("철갑옷", "강력한 방어구", 500, EquipmentSlot.Armor, 0, 20);
             case "전설갑옷":
